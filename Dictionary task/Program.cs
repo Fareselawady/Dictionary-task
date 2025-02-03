b9d6ddedd69de0c890cad692ccb185cb7928ea1c
Fareselawady <121154283+Fareselawady@users.noreply.github.com>
2025-02-03 22:22:43 +0200

    Dictionary Word Sorter
    Overview
    Dictionary Word Sorter is a C# console application that categorizes words based on their letters using a Dictionary<char, List<string>>. It processes a given text, extracts words, and organizes them into groups by their first letter, making it easier to analyze and retrieve words efficiently.
    
    Features
    Efficient Word Categorization: Groups words by their starting letter.
    Case Insensitive Processing: Converts all letters to lowercase to ensure uniformity.
    Dynamic Storage: Uses Dictionary<char, List<string>> to store words efficiently.
    Optimized Iteration: Ensures minimal processing overhead.
    How It Works
    The program takes a predefined text string.
    It splits the text into individual words.
    Each word is stored in a dictionary under the key of its first letter.
    The dictionary is then printed, displaying words categorized by their initial letter.

---
+++ Dictionary task/Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Dictionary_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var article = "the quick brown fox jumps over the lazy dog" + " i wish i was the monster that you think i am "
+                + "She likes to read books in the evening" + "He is learning English to speak with his friends";
+
+            Dictionary<char, List<string>> letters = new Dictionary<char, List<string>>();
+            foreach (var word in article.Split())
+            {
+                foreach (var letter in word)
+                {
+                    var c = char.ToLower(letter);
+                    if (!letters.ContainsKey(c))
+                    {
+                        letters.Add(c, new List<string> { word.ToLower() });
+
+                    }
+                    else
+                    {
+                        letters[c].Add(word);
+
+                    }
+                }
+            }
+            foreach (var item in letters)
+            {
+                Console.WriteLine($"'{item.Key}' : ");
+                foreach (var word in item.Value)
+                {
+                    Console.WriteLine($"\t \t {word}");
+                }
+            }
+
+
+
+        }
+    }
+
+}
